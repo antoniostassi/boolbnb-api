f17e5ddf167c01130bcc9f4b81d0732918f20492
Antonio Stassi <stassiant@outlook.it>
2024-11-20 11:43:06 +0100

    Models fillables update

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -7,4 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 class Message extends Model
 {
     //
+    protected $fillable = [
+        'content',
+        'user_email',
+        'firstname',
+        'lastname',
+        'apartment_id'
+    ];
 }
