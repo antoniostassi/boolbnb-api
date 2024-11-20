ed685dfc35145f85740505366e7567b65f3af560
AleScla <scla.alessandro@gmail.com>
2024-11-20 11:33:10 +0100

    Created a temporary route on api.php for the apartments controller

--- routes/api.php
+++ routes/api.php
@@ -2,10 +2,11 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\ApartmentController;
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
 
 
-
+Route::resource('apartment', ApartmentController::class);
