671a6fc9ebfb15d835df51f92eee2875fd45c994
AleScla <scla.alessandro@gmail.com>
2024-11-20 11:52:48 +0100

    Removed useless functions

---
+++ app/Http/Controllers/API/ApartmentController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers\API;
+// controllers
+use App\Http\Controllers\Controller;
+
+
+use App\Models\Apartment;
+use Illuminate\Http\Request;
+
+class ApartmentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Apartment $apartment)
+    {
+        //
+    }
+
+
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Apartment $apartment)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Apartment $apartment)
+    {
+        //
+    }
+}
