55edbf99320cd456297f0f367e24104864b38b25
Alessandro Sclafani <scla.alessandro@gmail.com>
2024-11-21 11:49:27 +0100

    Inizializzato seeder promotion - da testare

--- database/seeders/PromotionSeeder.php
+++ database/seeders/PromotionSeeder.php
@@ -4,14 +4,35 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+use App\Models\{
+    Promotion,
+};
 
 class PromotionSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
+    // ● 2,99 € per 24 ore di sponsorizzazione
+    // ● 5.99 € per 72 ore di sponsorizzazione
+    // ● 9.99 € per 144 ore di sponsorizzazione
     public function run(): void
     {
-        //
+        $promotions = [
+            [
+                'title' => 'Gold',
+                'description' => 'Questa sponsorizzazione offre 6 giorni di alta visibilità per il tuo annuncio',
+                'price' => 9.99,
+                'duration_time' => 0000-00-06,
+            ],
+        ];
+        Promotion::truncate();
+
+        DB::table('promotions')->insert([
+
+        ]);
+
     }
 }
