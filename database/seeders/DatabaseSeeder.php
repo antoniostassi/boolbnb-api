f7b2d52e939f65b7182890b0ba84612c3bd58498
Alessandro Sclafani <scla.alessandro@gmail.com>
2024-11-21 11:15:55 +0100

    changed position MessageSeeder based or its relationship

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,9 +22,9 @@ class DatabaseSeeder extends Seeder
         ]);
 
         $this->call([
-            MessageSeeder::class,
             PromotionSeeder::class,
             ApartmentSeeder::class,
+            MessageSeeder::class,
             StatisticSeeder::class,
             ServiceSeeder::class,
         ]);
