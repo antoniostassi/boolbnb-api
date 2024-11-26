59272e4ccf4e1d1ebe52b2f477d9e904eec0684e
Alessandro Sclafani <scla.alessandro@gmail.com>
2024-11-26 14:40:48 +0100

    removed useless functions, initialized store function

--- app/Http/Controllers/API/MessageController.php
+++ app/Http/Controllers/API/MessageController.php
@@ -2,36 +2,47 @@
 
 namespace App\Http\Controllers\Api;
 
+use Illuminate\Support\Facades\Validator;
+
+
 // controllers
 use App\Http\Controllers\Controller;
 
-use App\Models\Message;
+use App\Models\{
+    Message,
+    Apartment
+};
 use Illuminate\Http\Request;
 
 class MessageController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        //
-    }
 
     /**
      * Store a newly created resource in storage.
      */
     public function store(Request $request)
     {
-        //
-    }
+        $validator = Validator::make($request->all(), [
+            'content' => 'required|string|min:5',
+            'user_email'=> 'required|min:5|max:255',
+            'firstname' => 'nullable|min:3|max:64',
+            'lastname'=> 'nullable|min:3|max:64',
+            'apartment_id' => 'required|exists:apartments,id',
+        ]);
+        if($validator->fails()) {
+            $errors = $validator->errors();
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(Message $message)
-    {
-        //
-    }
+            return response()->json([
+                'errors' => $errors,
+                'message' => 'Si è verificato un errore inaspettato. Riprova più tardi.',
+            ], 400);
+        };
 
+        $message = Message::create($request->all());
+
+        return response()->json([
+            'status' => 'Il messaggio è stato inviato'
+        ], 200);
+
+    }
 }
