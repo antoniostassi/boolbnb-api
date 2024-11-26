212f86041d4fd3adf7fdb8f729118a06cfc5b2fe
Alessandro Sclafani <scla.alessandro@gmail.com>
2024-11-26 14:39:50 +0100

    defined route on api.php

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ApartmentController;
 use App\Http\Controllers\Api\ServiceController;
 use App\Http\Controllers\Api\PromotionController;
+use App\Http\Controllers\Api\MessageController;
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
@@ -19,10 +20,14 @@ Route::resource('promotions', PromotionController::class)->only([
     'index'
 ]);
 
-Route::resource('apartments', ApartmentController::class)->only([ 
+Route::resource('apartments', ApartmentController::class)->only([
     'index',
     'show',
     'store',
     'update',
     'destroy'
+]);
+
+Route::resource('messages', MessageController::class)->only([
+    'store',
 ]);
